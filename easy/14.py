515bf8fa40a5cd1532e7a002485c3b99932149ed
echo <lvaczuhc@gmail.com>
2020-07-18 08:47:18 +0800

    change 14 add method 2

--- easy/14.py
+++ easy/14.py
@@ -19,13 +19,17 @@ class Solution:
         for i in range(length):
             point = strs[0][i]
             if any(i == len(strs[j]) or strs[j] != point for j in range(1, count)):
-                return strs[0][:i]
+                return strs[0]
         return strs[0]
 
 
 if __name__ == "__main__":
     solution = Solution()
-    rom_number = ["c", "c", "c"]
-    result = solution.longestCommonPrefix2(strs=rom_number)
+
+    # strs = ["a", "ab"]
+    # strs = ["a"]
+    strs = ["a", "abc"]
+    # strs =    ["flower", "flow", "flight"]
+    result = solution.longestCommonPrefix2(strs=strs)
     print(type(result))
     print(str(result))
